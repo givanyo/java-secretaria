da801443e9088485a47296ba4591ef3807b1f530
Ra3lPimenta <pimenta.rafa.22@gmail.com>
2026-05-25 20:29:53 -0300

    Implement database connection in Conexao class
    Só o Nicollas colocar a URL , usario e senha 👍

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -1,5 +1,16 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
 
+    private static final String URL = "";
+    private static final String USUARIO = "";
+    private static final String SENHA = "";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
 }
