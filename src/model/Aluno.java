92f89df8b1e24a9f1771d6b40a833da00ddd1361
Ra3lPimenta <pimenta.rafa.22@gmail.com>
2026-05-25 20:25:29 -0300

    Add properties and methods to Aluno class

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -2,4 +2,102 @@ package model;
 
 public class Aluno {
 
+    private int id;
+    private String nomeAluno;
+    private String cpf;
+    private String dataNascimento;
+    private String genero;
+    private String nomeResponsavel;
+    private String afro;
+    private String escolaridadePublica;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nomeAluno, String cpf, String dataNascimento, String genero,
+                 String nomeResponsavel, boolean afro, boolean escolaridadePublica) {
+        this.nomeAluno = nomeAluno;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.genero = genero;
+        this.nomeResponsavel = nomeResponsavel;
+        this.afro = afro;
+        this.escolaridadePublica = escolaridadePublica;
+    }
+
+    public Aluno(int id, String nomeAluno, String cpf, String dataNascimento, String genero,
+                 String nomeResponsavel, String afro, String escolaridadePublica) {
+        this.id = id;
+        this.nomeAluno = nomeAluno;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.genero = genero;
+        this.nomeResponsavel = nomeResponsavel;
+        this.afro = afro;
+        this.escolaridadePublica = escolaridadePublica;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNomeResponsavel() {
+        return nomeResponsavel;
+    }
+
+    public void setNomeResponsavel(String nomeResponsavel) {
+        this.nomeResponsavel = nomeResponsavel;
+    }
+
+    public String isAfro() {
+        return afro;
+    }
+
+    public void setAfro(String afro) {
+        this.afro = afro;
+    }
+
+    public String isEscolaridadePublica() {
+        return escolaridadePublica;
+    }
+
+    public void setEscolaridadePublica(String escolaridadePublica) {
+        this.escolaridadePublica = escolaridadePublica;
+    }
 }
